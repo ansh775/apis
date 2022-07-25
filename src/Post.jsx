08786af37aad8c6113658b039a2f8c967703b342
kishanevery <kishan@every.lgbt>
2022-07-25 18:16:00 +0530

    Configure React app for deployment to GitHub Pages

---
+++ src/Post.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Post = () => {
+  return (
+    <>
+      <input type="text" placeholder="Enter Item Name" />
+      <button>Click</button>
+      {/* https://62d647f215ad24cbf2d39b56.mockapi.io/crud */}
+    </>
+  );
+};
+
+export default Post;
